47ec0f0a37e6884cd8a56c94f3408ed6f56f76a1
pedro <pedro.ltempo@gmail.com>
2026-03-09 22:14:05 -0300

    att

---
+++ Jogo_advinhacao.py
@@ -0,0 +1,29 @@
+from random import *
+control=0
+tentativas=0
+maximo=int(input("Digite o numero maximo para o jogo: "))
+numero = randint(1,maximo)
+palpite=int(input("Digite seu palpite: "))
+
+while palpite!=numero:
+    control+=1
+    if palpite>maximo:
+        print("palpite invalido, tente novamente")
+    
+    elif palpite<numero:
+        tentativas+=1
+        print("O numero é maior")
+    
+    else:
+        tentativas+=1
+        print("O numero é menor")
+    if tentativas==5:
+        print("Dica: O numero é par" if numero%2==0 else "Dica: O numero é impar")
+    if control%5==0:
+        if input("Deseja desistir? (s/n) ")=="s":
+            print(f"O numero era {numero}")
+            exit()
+    palpite=int(input("Digite seu palpite: "))
+
+tentativas+=1
+print(f"Parabens voce acertou o numero em {tentativas} tentativas")
